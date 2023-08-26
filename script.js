2ec86ef688e9a19e60b4c7d3074b89e5d96a8db4
jennieines <141196314+jennieines@users.noreply.github.com>
2023-08-26 13:07:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ var list = document.getElementById('todo-list'); // This variable stores the ele
 
 // The variables allow one to access and manipulate the form, input, and list elements in the JavaScript code. 
 
-window.addEventListener('load', function() { // This code adds an event listener to the window object that listens for the 'load' event.
+window.addEventListener('load', function () { //This code adds an event listener to the window object that listens for the 'load' event.
+    for (var i = 0; i < localStorage.length; i++) {
+      addTaskToList(localStorage.key(i)); // This loop retrieves each item from local storage and adds it to a list.
+    }
+    
     console.log('Page has loaded!'); // This code will be executed when the 'load' event occurs.
-});
+  });
+
+  
