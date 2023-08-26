a6c3b51e1b22b7ff67c45d91c36e337698b2d938
jennieines <141196314+jennieines@users.noreply.github.com>
2023-08-26 12:38:12 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var form = document.getElementById('todo-form'); //This variable stores the element with the ID "todo-form" from the index.html file.
+
+var input = document.getElementById('todo-input'); // This variable stores the element with the ID 'todo-input' from the index.html file.
+
+var list = document.getElementById('todo-list'); // This variable stores the element with the ID 'todo-list' from the index.html file.
+
+// The variables allow one to access and manipulate the form, input, and list elements in the JavaScript code. 
