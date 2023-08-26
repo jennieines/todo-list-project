8272aab5fdfa422d0e81d5e8a8740ca83f8ea595
jennieines <141196314+jennieines@users.noreply.github.com>
2023-08-26 12:45:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ var input = document.getElementById('todo-input'); // This variable stores the e
 var list = document.getElementById('todo-list'); // This variable stores the element with the ID 'todo-list' from the index.html file.
 
 // The variables allow one to access and manipulate the form, input, and list elements in the JavaScript code. 
+
+window.addEventListener('load', function() { // This code adds an event listener to the window object that listens for the 'load' event.
+    console.log('Page has loaded!'); // This code will be executed when the 'load' event occurs.
+});
